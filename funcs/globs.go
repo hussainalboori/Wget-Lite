9c1e3e9aa44a4585df7930d05e7b0c6c95e6e036
amali <e.amjad2010@gmail.com>
2024-01-19 13:05:26 +0300

    add globle Var

---
+++ funcs/globs.go
@@ -0,0 +1,10 @@
+package funcs
+
+import "github.com/spf13/pflag"
+
+var (
+	SaveAs    = pflag.StringP("O", "O", "", "save the downloaded file with a different name")
+	SaveDir   = pflag.StringP("P", "P", "", "directory to save the downloaded file")
+	RateLimit = pflag.Int64P("rate-limit", "R", 0, "limit the download speed in bytes per second")
+	InputFile = pflag.StringP("i", "i", "", "specify a file with paths, and the program will download them async style :)")
+)
