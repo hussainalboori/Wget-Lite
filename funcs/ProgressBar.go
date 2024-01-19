53bdcaecd973be5f3519fe6445cf41ec721edd10
amali <e.amjad2010@gmail.com>
2024-01-19 13:07:02 +0300

    fixed progress bar

--- funcs/ProgressBar.go
+++ funcs/ProgressBar.go
@@ -1,25 +1,10 @@
 package funcs
 
 import (
-	"fmt"
 	"io"
-	"time"
 
 	"github.com/schollz/progressbar/v3"
 )
-
-// Progress is a simple progress bar implementation
-type Progress struct {
-	Total   int64
-	Current int64
-	StopCh  chan struct{}
-}
-
-func (pr *ProgressReader) Read(p []byte) (n int, err error) {
-	n, err = pr.Reader.Read(p)
-	pr.Progress.Add64(int64(n))
-	return
-}
 
 // ProgressReader is a custom reader that updates progress
 type ProgressReader struct {
@@ -27,41 +12,54 @@ type ProgressReader struct {
 	Progress *progressbar.ProgressBar
 }
 
-func (p *Progress) Display() {
-	ticker := time.NewTicker(time.Millisecond * 100)
-	defer ticker.Stop()
+// func (pr *ProgressReader) Read(p []byte) (n int, err error) {
+// 	n, err = pr.Reader.Read(p)
+// 	pr.Progress.Add64(int64(n))
+// 	return
+// }
 
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Printf("\r%6.2f KiB / %6.2f KiB [%-50s] %.2f%% %.2f MiB/s",
-				float64(p.Current)/1024, float64(p.Total)/1024, getProgressBar(p), float64(p.Current)/(1024*1024), float64(p.Current)/(1024*1024)/(time.Since(startTime).Seconds()))
-		case <-p.StopCh:
-			return
-		}
-	}
-}
+// // Progress is a simple progress bar implementation
+// type Progress struct {
+// 	Total   int64
+// 	Current int64
+// 	StopCh  chan struct{}
+// }
 
-func (p *Progress) Write(b []byte) (n int, err error) {
-	n = len(b)
-	p.Current += int64(n)
-	return n, nil
-}
+// func (p *Progress) Display() {
+// 	ticker := time.NewTicker(time.Millisecond * 100)
+// 	defer ticker.Stop()
 
-func (p *Progress) Stop() {
-	close(p.StopCh)
-}
+// 	for {
+// 		select {
+// 		case <-ticker.C:
+// 			fmt.Printf("\r%6.2f KiB / %6.2f KiB [%-50s] %.2f%% %.2f MiB/s",
+// 				float64(p.Current)/1024, float64(p.Total)/1024, getProgressBar(p), float64(p.Current)/(1024*1024), float64(p.Current)/(1024*1024)/(time.Since(startTime).Seconds()))
+// 		case <-p.StopCh:
+// 			return
+// 		}
+// 	}
+// }
 
-func getProgressBar(p *Progress) string {
-	width := 50
-	progress := int((float64(p.Current) / float64(p.Total)) * float64(width))
-	bar := make([]rune, width)
-	for i := 0; i < width; i++ {
-		if i < progress {
-			bar[i] = '='
-		} else {
-			bar[i] = ' '
-		}
-	}
-	return string(bar)
-}
+// func (p *Progress) Write(b []byte) (n int, err error) {
+// 	n = len(b)
+// 	p.Current += int64(n)
+// 	return n, nil
+// }
+
+// func (p *Progress) Stop() {
+// 	close(p.StopCh)
+// }
+
+// func getProgressBar(p *Progress) string {
+// 	width := 50
+// 	progress := int((float64(p.Current) / float64(p.Total)) * float64(width))
+// 	bar := make([]rune, width)
+// 	for i := 0; i < width; i++ {
+// 		if i < progress {
+// 			bar[i] = '='
+// 		} else {
+// 			bar[i] = ' '
+// 		}
+// 	}
+// 	return string(bar)
+// }
