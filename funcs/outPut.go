8c0800e8f84885ac83150e68db9395f8ae844d55
amali <e.amjad2010@gmail.com>
2024-01-20 15:44:23 +0300

    show response.StatusCode and append to existing log

--- funcs/outPut.go
+++ funcs/outPut.go
@@ -16,11 +16,22 @@ func Output(response *http.Response, file *os.File, savePath, url string) error 
 
 	contentSize := response.ContentLength
 
+	var responseMessage string
+
+	switch response.StatusCode {
+	case http.StatusOK:
+		responseMessage = "Status 200 OK."
+	case http.StatusForbidden:
+		responseMessage = "Access denied. Status 403 Forbidden."
+	default:
+		responseMessage = fmt.Sprintf("Unexpected status code: %d %s", response.StatusCode, http.StatusText(response.StatusCode))
+	}
+
 	if *SilentMode {
 		fmt.Println("Logs will be written to Wget-light-log.txt")
 
-		// Open or create a log file
-		logFile, err := os.Create("Wget-light-log.txt")
+		// Open or create a log file in append mode
+		logFile, err := os.OpenFile("Wget-light-log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			return err
 		}
@@ -31,7 +42,7 @@ func Output(response *http.Response, file *os.File, savePath, url string) error 
 
 		// Log the output
 		log.Printf("start at %s", startTime.Format("2006-01-02 15:04:05"))
-		log.Printf("Sending request, awaiting response... status %d OK", response.StatusCode)
+		log.Printf("Sending request, awaiting response... %s", responseMessage)
 		log.Printf("Content size: %d [~%.2fMB]", response.ContentLength, float64(response.ContentLength)/(1024*1024))
 		log.Printf("Saving file to: %s", savePath)
 
@@ -44,15 +55,14 @@ func Output(response *http.Response, file *os.File, savePath, url string) error 
 
 		// Log the download completion
 		log.Printf("Downloaded [%s]", url)
-		log.Printf("Finished at %s", time.Now().Format("2006-01-02 15:04:05"))
+		log.Print("Finished at ", time.Now().Format("2006-01-02 15:04:05"), "\n\n")
 	} else {
 
-		//////////////////////////////////////////////////////////////////////////////
 		fmt.Printf("start at %s\n", startTime.Format("2006-01-02 15:04:05"))
-		fmt.Printf("sending request, awaiting response... status %d OK\n", response.StatusCode)
+		fmt.Printf("sending request, awaiting response... %s\n", responseMessage)
 		fmt.Printf("content size: %d [~%.2fMB]\n", contentSize, float64(contentSize)/(1024*1024))
 		fmt.Printf("saving file to: %s\n", savePath)
-		//////////////////////////////////////////////////////////////////////////////
+
 		// Create a progress progress
 		progress := progressbar.DefaultBytes(
 			contentSize,
