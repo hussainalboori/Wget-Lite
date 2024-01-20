de71d26c924c46448bfd8b94b23132a703cb7090
amali <e.amjad2010@gmail.com>
2024-01-20 18:05:13 +0300

    now we mimic a real browser when sending HTTP request

--- funcs/download.go
+++ funcs/download.go
@@ -10,6 +10,33 @@ import (
 
 func DownloadFile(url string, wg *sync.WaitGroup) error {
 	defer wg.Done()
+	// Make the HTTP request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	// Set a common User-Agent header
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+
+	// Follow redirects
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			// Allow redirects
+			return nil
+		},
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	// Check if the response is successful
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error2: HTTP status code %d", response.StatusCode)
+	}
 
 	// Create the save directory if it doesn't exist
 	if *SaveDir != "" {
@@ -28,20 +55,6 @@ func DownloadFile(url string, wg *sync.WaitGroup) error {
 		os.Exit(1)
 	}
 	defer file.Close()
-
-	// Make the HTTP request
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error1:", err)
-
-		os.Exit(1)
-	}
-	defer response.Body.Close()
-
-	// if response.StatusCode != http.StatusOK {
-	// 	fmt.Printf("\nError2: HTTP status code %d\n", response.StatusCode)
-	// 	os.Exit(1)
-	// }
 
 	Output(response, file, savePath, url)
 
